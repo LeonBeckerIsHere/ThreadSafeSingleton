18bc7d13f4239b43f007c693b129a577f9d599f1
Leon Becker <lab160730@utdallas.edu>
2019-01-31 15:07:09 -0600

    First commit

---
+++ ThreadSafeSingleton.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "ThreadSafeSingleton.h"
+
+ThreadSafeSingleton& ThreadSafeSingleton::getInstance(){
+ //Once this method is called the single instance of ThreadSafeSingleton is created
+ //Static data initializes only once inside of a method
+ static ThreadSafeSingleton instance;
+ return instance;
+}
+
+std::string ThreadSafeSingleton::getObjectMessage() {
+	return message;
+}
+
+void ThreadSafeSingleton::setObjectMessage(std::string inputMessage) {
+	message = inputMessage;
+}
---
+++ ThreadSafeSingleton.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "stdafx.h"
+
+class ThreadSafeSingleton {
+public:
+	static ThreadSafeSingleton& getInstance();
+
+	//Stop the Singletoon from being copied or moved
+	ThreadSafeSingleton(ThreadSafeSingleton const&) = delete;
+	ThreadSafeSingleton(ThreadSafeSingleton&&) = delete;
+	ThreadSafeSingleton& operator=(ThreadSafeSingleton const&) = delete;
+	ThreadSafeSingleton& operator=(ThreadSafeSingleton &&) = delete;
+
+	//Generic functionality added to ensure the TSSingleton is working
+	std::string getObjectMessage();
+	void setObjectMessage(std::string inputMessage);
+
+private:
+	//Stop ThreadSafeSingleton from being contructed or deconstructed outside of the class
+	ThreadSafeSingleton() {}
+	~ThreadSafeSingleton() {}
+
+	std::string message;
+};
